7fed40b4c45e40b73442c7c10a81873d38de1306
Ana P. Melo <152793670+An4PDM@users.noreply.github.com>
2025-02-05 18:23:46 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ DB_HOST = 'host_example'
 DB_USER = 'user_example'
 DB_PASSWORD = 'password_example'
 DB_NAME = 'vendas_dataco'
+
+# Slack configuration
+SLACK_TOKEN = "slack_token"
+SLACK_CHANNEL = "#your_notification_channel"
